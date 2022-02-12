4a591e662e348eda94ab4314721cc60e2d0a0cfa
rachit <rachau76@gmail.com>
2022-02-12 16:58:53 +0530

    made the basic layout of the activity's page

--- lib/screens/tasks/task_page.dart
+++ lib/screens/tasks/task_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pax/theme.dart';
 
 class TasksPage extends StatefulWidget {
   const TasksPage({Key? key}) : super(key: key);
@@ -10,26 +11,102 @@ class TasksPage extends StatefulWidget {
 late Map arguments;
 
 class _TasksPageState extends State<TasksPage> {
+  final _theme = OurTheme();
   @override
   Widget build(BuildContext context) {
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
     arguments = ModalRoute.of(context)!.settings.arguments as Map;
     return Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: const Text("task"),
+          iconTheme: IconThemeData(color: Colors.black),
+          title: Text(
+            "Your Today's Activity",
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                fontSize: 22.0,
+                fontFamily: _theme.font,
+                letterSpacing: 1.2),
+          ),
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          elevation: 0.0,
         ),
         body: Center(
-          child: Container(
-              child: Column(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text(arguments['task']),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/post_page',
-                        arguments: {'task': arguments['task']});
-                  },
-                  child: Text(arguments['task']))
+              SizedBox(
+                width: _width * 0.9,
+                child: Text(
+                  arguments['task'],
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: _theme.font,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25.0),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.grey[800]),
+                height: _height * 0.35,
+                width: _width * 0.95,
+                child: const Center(
+                  child: Text(
+                    "Sample image about the acivity will come here",
+                    style: TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: _width * 0.9,
+                child: const Text(
+                  "Here some brief description about the activity will come\nBla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla",
+                  style: TextStyle(color: Colors.black, fontSize: 20.0),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Text(
+                    'Click to see YT video',
+                    style: TextStyle(
+                        color: Colors.blueAccent,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    'Click to see loactions near you',
+                    style: TextStyle(
+                        color: Colors.blueAccent,
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/post_page',
+                            arguments: {'task': arguments['task']});
+                      },
+                      child: const Text("Skip todays activity")),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/post_page',
+                            arguments: {'task': arguments['task']});
+                      },
+                      child: const Text("Post your activity")),
+                ],
+              )
             ],
-          )),
+          ),
         ));
   }
 }
