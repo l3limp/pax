755e60116f33703598cb31e4a32e37f9756479e6
rachit <rachau76@gmail.com>
2022-02-13 13:05:23 +0530

    made the UI for the tasks page

--- lib/screens/tasks/task_page.dart
+++ lib/screens/tasks/task_page.dart
@@ -49,34 +49,55 @@ class _TasksPageState extends State<TasksPage> {
         backgroundColor: Colors.white,
         appBar: AppBar(
           iconTheme: const IconThemeData(color: Colors.black),
-          title: Text(
-            "Your Today's Activity",
+          title: const Text(
+            "Activity of the Day",
             style: TextStyle(
                 color: Colors.black,
-                fontWeight: FontWeight.w600,
+                fontWeight: FontWeight.w500,
                 fontSize: 22.0,
-                fontFamily: _theme.font,
                 letterSpacing: 1.2),
           ),
           backgroundColor: Colors.white,
-          centerTitle: true,
           elevation: 0.0,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(7.0),
+              child: InkWell(
+                onTap: () async {
+                  int number = _random.nextInt(7);
+                  await setTask(number);
+                  updateScreen(number);
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFF585c),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "Skip",
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      ),
+                      Icon(
+                        Icons.arrow_right,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                    ],
+                  ),
+                  width: _width * 0.22,
+                  height: 25.0,
+                ),
+              ),
+            ),
+          ],
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              SizedBox(
-                width: _width * 0.9,
-                child: Text(
-                  _tasks.tasksList[taskNumber],
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: _theme.font,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25.0),
-                ),
-              ),
               Container(
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15.0),
@@ -93,11 +114,44 @@ class _TasksPageState extends State<TasksPage> {
               ),
               SizedBox(
                 width: _width * 0.9,
+                child: Text(
+                  _tasks.tasksList[taskNumber],
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w700,
+                      letterSpacing: 0.4,
+                      fontSize: 27.0),
+                ),
+              ),
+              SizedBox(
+                width: _width * 0.9,
                 child: const Text(
                   "Here some brief description about the activity will come\nBla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla Bla",
                   style: TextStyle(color: Colors.black, fontSize: 20.0),
                 ),
               ),
+              InkWell(
+                  onTap: () async {
+                    if (!await launch(_googleLinks.googleLinks[taskNumber])) {
+                      throw 'Could not launch URL';
+                    }
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.location_on,
+                        color: Color(0xFFFF585C),
+                      ),
+                      Text(
+                        'Click to see loactions near you',
+                        style: TextStyle(
+                            color: Color(0xFFFF585C),
+                            fontSize: 17.5,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  )),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
@@ -107,46 +161,59 @@ class _TasksPageState extends State<TasksPage> {
                         throw 'Could not launch URL';
                       }
                     },
-                    child: const Text(
-                      'Click to see YT video',
-                      style: TextStyle(
-                          color: Colors.blueAccent,
-                          fontSize: 15.0,
-                          fontWeight: FontWeight.w600),
+                    child: Container(
+                      width: _width * 0.4,
+                      height: _height * 0.05,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: const Color(0xFFFF585C),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const [
+                          Icon(
+                            Icons.desktop_mac_rounded,
+                            color: Colors.white,
+                            size: 25.0,
+                          ),
+                          Text(
+                            "Watch Demo",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 20.0),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                   InkWell(
                     onTap: () async {
-                      if (!await launch(_googleLinks.googleLinks[taskNumber])) {
-                        throw 'Could not launch URL';
-                      }
+                      Navigator.pushNamed(context, '/write_post',
+                          arguments: {'task': _tasks.tasksList[taskNumber]});
                     },
-                    child: const Text(
-                      'Click to see loactions near you',
-                      style: TextStyle(
-                          color: Colors.blueAccent,
-                          fontSize: 15.0,
-                          fontWeight: FontWeight.w600),
+                    child: Container(
+                      width: _width * 0.4,
+                      height: _height * 0.05,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: const Color(0xFFFF585C),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const [
+                          Icon(
+                            Icons.add_circle_rounded,
+                            color: Colors.white,
+                            size: 25.0,
+                          ),
+                          Text(
+                            "Add Post",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 20.0),
+                          )
+                        ],
+                      ),
                     ),
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                      onPressed: () async {
-                        int number = _random.nextInt(7);
-                        await setTask(number);
-                        updateScreen(number);
-                      },
-                      child: const Text("Skip todays activity")),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/write_post',
-                            arguments: {'task': _tasks.tasksList[taskNumber]});
-                      },
-                      child: const Text("Post your activity")),
+                  ),
                 ],
               )
             ],
