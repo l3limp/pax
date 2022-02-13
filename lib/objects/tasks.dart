db9d7173e8a428e105b523217f4ebe916cbce931
l3limp <kakarvardaan@gmail.com>
2022-02-13 20:37:14 +0530

    added description

--- lib/objects/tasks.dart
+++ lib/objects/tasks.dart
@@ -6,9 +6,21 @@ class Tasks {
     "Bake a cake and some muffins",
     "Make a short video/movie about your home",
     "Read your favourite book in a park",
-    "Build a blanket and pillow fort , decorate it, and watch your favourtie movie",
+    "Watch a movie in a pillow fort",
     "Go camping with friends",
     "Go for a swim",
-    "Have a water baloon fight",
+    "Have a water balloon fight",
+  ];
+  List<String> tasksDescription = [
+    "Get up early in the morning, with some friends who jog regularly, and jog for atleast 4 kms. Sweat it out and have a refreshing bath with cold water later",
+    "Find a hiking/trekking place nearby, and go on a solo trek. Enjoy and embrace the beauty nature has to offer. Talk to fellow hikers and make new friends",
+    "Go on a solo picnic in a cool, shady place in the lap of nature. Pack a meal and sleep under the trees for atleast an hour",
+    "Take out your mittens, heat your oven and bake something delicious! Make fresh fruit juice. Invite your neighbours over and enjoy",
+    "Try out your hand at movie-making, and record every little detail about your home, commenting about everything, so you can relive the memories in the future",
+    "Go to a cool, shady place in the lap of nature. Pack a meal, your favourite book and READ! Stay out until evening",
+    "Build a blanket and pillow fort , decorate it, and watch your favourtie movie in the cosy and nostalgic place",
+    "Take out a day to go camping to a nearby place with school friends in the lap of nature. Trek, eat and have fun!",
+    "Find a swimming pool nearby, and rush to it to take a refreshing swim in the cold water after a hectic day. Swim until you cant anymore",
+    "Relive your childhood and Holi days! Gather neighbouring kids and have a water balloon fight with them. Get a water gun and drown the kids mercilessly",
   ];
 }
